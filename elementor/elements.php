6bd34024960e565ce0ac1c360d08576360da1d12
prpb-github <parsa.rpb20011@gmail.com>
2025-04-12 16:32:32 +0330

    Delete elementor directory

--- elementor/elements.php
+++
@@ -1,9 +0,0 @@
-<?php
-function register_mok_widget( $widgets_manager ) {
-
-	require_once( __DIR__ . '/widgets/mok-widget-1.php' );
-    require_once( __DIR__ . '/widgets/mok-widget-2.php' );
-	$widgets_manager->register( new \elementor_mok_widget_1() );
-    $widgets_manager->register( new \elementor_mok_widget_2() );
-}
-add_action( 'elementor/widgets/register', 'register_mok_widget' );
