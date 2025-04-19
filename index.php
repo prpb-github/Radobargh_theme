6ac61df20a83ccae4b5704bbd798706399b0af86
prpb-github <parsa.rpb20011@gmail.com>
2025-04-19 16:18:55 +0330

    Update index.php

--- index.php
+++ index.php
@@ -112,43 +112,4 @@
         </div>
           </div>
           </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <div class="teacher-informations">
-        <img src="#" alt="">
-        <h2>test</h2>
-        <h4>test</h4>
-      </div>
-    </div>
-    <div class="carousel-item">
-      <img src="..." class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="..." class="d-block w-100" alt="...">
-    </div>
-  </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
-</div>
 <?php get_footer() ?>
